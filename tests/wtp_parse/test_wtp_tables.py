9f8e30580fbb7f6194d995a0c307fdb429c8b364
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-07-01 05:09:46 +0300

    .

--- tests/wtp_parse/test_wtp_tables.py
+++ tests/wtp_parse/test_wtp_tables.py
@@ -1,71 +1,77 @@
-import sys
-from pathlib import Path
-sys.path.append(str(Path(__file__).parent.parent.parent))
+
+
+import pytest
 
 from src.wtp_parse.wtp_tables import (
     update_wikitable_data,
 )
 
+class TestUpdate:
 
-rows = {
-    "Hardware donation program/EYo237": {
-        "page_link": "Hardware donation program/EYo237",
-        "age": "25",
-        "home_wiki": "test",
-        "approved": "",
-    }
-}
+    @pytest.fixture(autouse=True)
+    def setup(self) -> None:
+        self.rows = {
+            "Hardware donation program/EYo237": {
+                "page_link": "Hardware donation program/EYo237",
+                "age": "25",
+                "home_wiki": "test",
+                "approved": "",
+            }
+        }
 
-wikitext = """
-{| class="wikitable sortable"
-! Page !! Age of account !! Home Wiki !! Approved
-|-
-| [[Hardware donation program/EYo237 ]]
-|
-|
-| zz
-|-
-"""
+        self.wikitext = (
+            '{| class="wikitable sortable"\n'
+            '! Page !! Age of account !! Home Wiki !! Approved\n'
+            '|-\n'
+            '| [[Hardware donation program/EYo237 ]]\n'
+            '|\n'
+            '|\n'
+            '| zz\n'
+            '|-\n'
+            '|}'
+        )
 
-table_headers_to_row_key = {
-    "Page": "page_link",
-    "Age of account": "age",
-    "Home Wiki": "home_wiki",
-}
+        self.table_headers_to_row_key = {
+            "Page": "page_link",
+            "Age of account": "age",
+            "Home Wiki": "home_wiki",
+        }
 
-def test_update_wikitable_data() -> None:
-    retult = update_wikitable_data(
-        rows,
-        wikitext,
-        table_headers_to_row_key,
-    )
-    expected_wikitext = (
-        '{| class="wikitable sortable"\n'
-        '! Page !! Age of account !! Home Wiki !! Approved\n'
-        '|-\n'
-        '| [[Hardware donation program/EYo237 ]]\n'
-        '| 25\n'
-        '| test\n'
-        '| zz\n'
-        '|-'
-    )
-    assert retult.strip() == expected_wikitext
+    def test_update_wikitable_data(self) -> None:
+        retult = update_wikitable_data(
+            self.rows,
+            self.wikitext,
+            self.table_headers_to_row_key,
+        )
+        expected_wikitext = (
+            '{| class="wikitable sortable"\n'
+            '! Page !! Age of account !! Home Wiki !! Approved\n'
+            '|-\n'
+            '| [[Hardware donation program/EYo237 ]]\n'
+            '| 25\n'
+            '| test\n'
+            '| zz\n'
+            '|-\n'
+            '|}'
+        )
+        assert retult.strip() == expected_wikitext
 
-def test_update_wikitable_data_replace_values() -> None:
-    retult = update_wikitable_data(
-        rows,
-        wikitext,
-        table_headers_to_row_key,
-        replace_values=True,
-    )
-    expected_wikitext = (
-        '{| class="wikitable sortable"\n'
-        '! Page !! Age of account !! Home Wiki !! Approved\n'
-        '|-\n'
-        '| Hardware donation program/EYo237\n'
-        '| 25\n'
-        '| test\n'
-        '| zz\n'
-        '|-'
-    )
-    assert retult.strip() == expected_wikitext
+    def test_update_wikitable_data_replace_values(self) -> None:
+        retult = update_wikitable_data(
+            self.rows,
+            self.wikitext,
+            self.table_headers_to_row_key,
+            replace_values=True,
+        )
+        expected_wikitext = (
+            '{| class="wikitable sortable"\n'
+            '! Page !! Age of account !! Home Wiki !! Approved\n'
+            '|-\n'
+            '| Hardware donation program/EYo237\n'
+            '| 25\n'
+            '| test\n'
+            '| zz\n'
+            '|-\n'
+            '|}'
+        )
+        assert retult.strip() == expected_wikitext
