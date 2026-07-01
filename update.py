e01a27d0923bb7171e21e4b5c4a1fb5a4bc3e85d
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-07-01 03:45:12 +0300

    Update update.py

--- update.py
+++ update.py
@@ -27,10 +27,16 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+page_title = "User:Mr. Ibrahem/hdp"
+output_file_name = "Mr. Ibrahem_hdp.wiki"
+
+page_title = "User:Mr. Ibrahem/test"
+output_file_name = "test.wiki"
+
 if __name__ == "__main__":
     update(
-        page_title="User:Mr. Ibrahem/hdp",
-        output_file_name="Mr. Ibrahem_hdp.wiki",
+        page_title=page_title,
+        output_file_name=output_file_name,
         unknown_placeholder="",
         load_recent_editcounts=False,
     )
