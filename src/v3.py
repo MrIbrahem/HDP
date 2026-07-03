3023f1923556bb146030f79c664d5256548209d4
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-07-04 02:45:55 +0300

    Update v3.py

--- src/v3.py
+++ src/v3.py
@@ -80,7 +80,7 @@ def solve_users_redirects(api: MwclientApi, data) -> list[dict[str, str]]:
         if users_redirects_api.get(user_str):
             x["username"] = users_redirects_api[user_str].removeprefix("User:")
 
-            if username == "User:Johnjoy12":
+            if user_str == "User:Johnjoy12":
                 logger.info(f"Johnjoy12 is a redirect to {x["username"]}")
 
         new_data.append(x)
